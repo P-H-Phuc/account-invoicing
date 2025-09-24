a9dbb030f92f6d7825c796fb781d267c7763562e
phucph <phucph@trobz.com>
2025-09-24 09:57:26 +0700

    [IMP] account_invoice_refund_option: pre-commit auto fixes

--- account_invoice_refund_option/models/account_invoice_refund.py
+++ account_invoice_refund_option/models/account_invoice_refund.py
@@ -6,24 +6,23 @@ class AccountInvoiceRefund(models.TransientModel):
 
     @api.model
     def _get_domain_invoice_line(self):
-        invoice_id = self._context.get('active_id', False)
-        return [('invoice_id', '=', invoice_id)]
+        invoice_id = self._context.get("active_id", False)
+        return [("invoice_id", "=", invoice_id)]
 
     filter_refund = fields.Selection(
-        selection_add=[('refund_select_product',
-                        'Select product(s) for refunding')])
+        selection_add=[("refund_select_product", "Select product(s) for refunding")]
+    )
     invoice_line_ids = fields.Many2many(
-        'account.invoice.line', string='Invoice Lines',
-        domain=_get_domain_invoice_line)
+        "account.invoice.line", string="Invoice Lines", domain=_get_domain_invoice_line
+    )
 
     @api.multi
-    def compute_refund(self, mode='refund'):
-        if self.filter_refund == 'refund_select_product':
-            self = self.with_context(
-                invoice_lines=self.invoice_line_ids or [])
-        return super(AccountInvoiceRefund, self).compute_refund(mode=mode)
+    def compute_refund(self, mode="refund"):
+        if self.filter_refund == "refund_select_product":
+            self = self.with_context(invoice_lines=self.invoice_line_ids or [])
+        return super().compute_refund(mode=mode)
 
-    @api.onchange('filter_refund')
+    @api.onchange("filter_refund")
     def onchange_select_product(self):
-        if self.filter_refund != 'refund_select_product':
+        if self.filter_refund != "refund_select_product":
             self.invoice_line_ids = []
